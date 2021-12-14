01c419e3f6847fa1d9b454df6c35233480ce9fe0
Sushant.Sinha <sushant1058jan@gmail.com>
2021-12-14 13:02:43 +0530

    changes made

---
+++ pan.py
@@ -0,0 +1,19 @@
+# import pandas as pd
+import pandas as pd
+ 
+# # list of strings
+# lst = ['Geeks', 'For', 'Geeks', 'is', 
+#             'portal', 'for', 'Geeks']
+ 
+# # Calling DataFrame constructor on list
+# df = pd.DataFrame(lst)
+# print(df)
+
+data = pd.read_csv("nba.csv")
+print(data)
+
+print('##########################')
+
+five=data.head()
+print(five)
+
