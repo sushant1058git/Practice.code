e7c47cad26e2753ca620e2baaf36278ddee7b8e7
Sushant.Sinha <sushant1058jan@gmail.com>
2021-12-14 22:23:02 +0530

    changes made

--- pan.py
+++ pan.py
@@ -1,18 +1,26 @@
+
 # import pandas as pd
+# data = pd.read_csv("nba.csv")
+
+# data.dropna(inplace=True)           #Removing null values to remove errors
+
+# perc=[.2,.4,.6,.8]                  #Defining percentage values
+# include=['object','float','int']    #List of dtypes to include    
+# desc=data.describe(percentiles=perc,include=include) #calling describe function
+# print(desc)
+
+
+# import pandas as pd
+
+# data=pd.read_csv('nba.csv')
+# # print(data)
+# data.dropna(inplace=True)
+# desc=data["Name"].describe()
+# print(desc)
+
 import pandas as pd
- 
-# # list of strings
-# lst = ['Geeks', 'For', 'Geeks', 'is', 
-#             'portal', 'for', 'Geeks']
- 
-# # Calling DataFrame constructor on list
-# df = pd.DataFrame(lst)
-# print(df)
 
-data = pd.read_csv("nba.csv")
-print(data)
 
-print('##########################')
 
 
 
