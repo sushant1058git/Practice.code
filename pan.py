94d0b6850f73fc5ac017d13297f7f19cb8b9f160
Sushant.Sinha <sushant1058jan@gmail.com>
2021-12-23 23:01:28 +0530

    changes made

--- pan.py
+++ pan.py
@@ -369,31 +369,131 @@
  
 # print(res1)
 
-'''Using Join'''
+# '''Using Join'''
 
-# importing pandas module
-import pandas as pd 
+# # importing pandas module
+# import pandas as pd 
  
-# Define a dictionary containing employee data 
-data1 = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'], 
-        'Age':[27, 24, 22, 32]} 
+# # Define a dictionary containing employee data 
+# data1 = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'], 
+#         'Age':[27, 24, 22, 32]} 
    
-# Define a dictionary containing employee data 
-data2 = {'Address':['Allahabad', 'Kannuaj', 'Allahabad', 'Kannuaj'], 
-        'Qualification':['MCA', 'Phd', 'Bcom', 'B.hons']} 
+# # Define a dictionary containing employee data 
+# data2 = {'Address':['Allahabad', 'Kannuaj', 'Allahabad', 'Kannuaj'], 
+#         'Qualification':['MCA', 'Phd', 'Bcom', 'B.hons']} 
  
-# Convert the dictionary into DataFrame  
-df = pd.DataFrame(data1,index=['K0', 'K1', 'K2', 'K3'])
+# # Convert the dictionary into DataFrame  
+# df = pd.DataFrame(data1,index=['K0', 'K1', 'K2', 'K3'])
  
-# Convert the dictionary into DataFrame  
-df1 = pd.DataFrame(data2, index=['K0', 'K2', 'K3', 'K4'])
+# # Convert the dictionary into DataFrame  
+# df1 = pd.DataFrame(data2, index=['K0', 'K2', 'K3', 'K4'])
 
 
-print(df, "\n\n", df1)  
+# print(df, "\n\n", df1)  
 
-res = df.join(df1)
+# res = df.join(df1)
  
-print(res)
+# print(res)
+
+# '''Creating a dates dataframe'''
+
+# import pandas as pd
+# import datetime
+ 
+# # Create dates dataframe with frequency 
+# data = pd.date_range('1/1/2011', periods = 10, freq ='H')
+ 
+# print(data)
+
+# x=datetime.datetime.now()
+# print(x.month, x.year)
+
+
+'''Working with text in pandas'''
+
+# '''code 1'''
+
+# import pandas as pd
+
+# data = {'Name':['sushant','ajay', 'vijay', 'raj','vikram'],
+#         'Age':[23,34,23,45,23]}
+
+# df=pd.DataFrame(data)
+# print(df)
+# print()
+
+# print("Converting to uppercase \n")
+
+# df['Name']=df['Name'].str.upper()
+# print(df)
+
+'''Code 1'''
+
+# # importing pandas module  
+# import pandas as pd 
+     
+# # Define a dictionary containing employee data 
+# data = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'], 
+#         'Age':[27, 24, 22, 32], 
+#         'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Knnuaj'], 
+#         'Qualification':['Msc', 'MA', 'MCA', 'Phd']} 
+ 
+# # Convert the dictionary into DataFrame  
+# df = pd.DataFrame(data) 
+    
+# # dropping null value columns to avoid errors 
+# df.dropna(inplace = True) 
+    
+# # new data frame with split value columns 
+# df["Address"]= df["Address"].str.split("a",n=0,expand=False) 
+   
+# # df display  
+# print(df)
+
+
+# '''Code #2:'''
+
+# # importing pandas module 
+# import pandas as pd
+ 
+# # reading csv file from url
+# data = pd.read_csv("nba.csv")
+ 
+# # overwriting column with replaced value of age
+# data["Age"]= data["Age"].replace(25.0, "Twenty five")
+ 
+# # creating a filter for age column 
+# # where age = "Twenty five"
+# # filter = data["Age"]=="Twenty five"
+ 
+# # printing only filtered columns
+# print(data.where(data['Age']=='Twenty five').dropna())
+
+
+'''Code #1:'''
+
+# # importing pandas module 
+# import pandas as pd 
+   
+# # Define a dictionary containing employee data 
+# data = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'], 
+#         'Age':[27, 24, 22, 32], 
+#         'Address':['Nagpur', 'Kanpur', 'Allahabad', 'Kannuaj'], 
+#         'Qualification':['Msc', 'MA', 'MCA', 'Phd']} 
+ 
+# # Convert the dictionary into DataFrame  
+# df = pd.DataFrame(data) 
+ 
+# # making copy of address column 
+# new = df["Address"].copy() 
+   
+# # concatenating address with name column 
+# # overwriting name column 
+# df["Name"]= df["Name"].str.cat(new, sep =", ") 
+   
+# # display 
+# print(df)
+
 
 
 
