77515afd8277501a5217602dd0955958cb6cc6cb
Sushant.Sinha <sushant1058jan@gmail.com>
2021-12-30 20:26:33 +0530

    changes made

---
+++ Javascript/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+let js = 'amazing'
+if (js === 'amazing') alert('js is fun')
+console.log("Hello Sushant")
+
+let firstname = 'sinha'
+console.log(firstname)
+console.log(firstname)
---
+++ Javascript/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,32 @@
+// 'use strict';
+// let hasdriverslicsence=false;
+
+// const driver=true
+// if (driver) hasdriverslicsence=true
+
+// if (hasdriverslicsence) alert("great ! you got drivers liscence");
+
+//Function declaration
+// function myfunc(apples, oranges){
+//     console.log(apples, oranges)
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice
+// }
+
+// var res=myfunc(2,3)
+// console.log(res)
+
+// const res1=myfunc(3,6)
+// console.log(res1)
+
+
+
+
+// function birthCal(birthYear){
+//     return 2037-birthYear
+// }
+
+// res=birthCal(1994)
+// console.log(res)
+
+//Arrow Function is helpful fro one liner function
