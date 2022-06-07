9a05fd65eb6061adc64d5af80697a44ffee21c0b
Sushant.sinha_94 <sushant1058jan@gmail.com>
2022-06-07 20:03:16 +0530

    leetcode

---
+++ DSA/LeetCode.py
@@ -0,0 +1,37 @@
+#1. Two Sum
+nums=[2,7,11,15]
+target=9
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+                
+c=Solution()
+print(c.twoSum(nums,target))
+
+#2. Palindrome Number
+class Solution:
+    def isPalindrome(self, x):
+        if str(x)==str(x)[::-1]:
+            return True
+        
+c=Solution()
+print(c.isPalindrome(121))
+
+
+#3. Container With Most Water
+height=[2,3,4,5,6]
+class Solution:
+    def maxArea(self, height) -> int:
+        max_area=0
+        area=0
+        for i in range(0,len(height)):
+            for j in range(i+1,len(height)):
+                area=min(height[i],height[j])*(j-i)
+                max_area=max(max_area,area)
+        return max_area
+            
+c=Solution()
+print(c.maxArea(height))
