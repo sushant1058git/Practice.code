74d17a693026d73678edfe8646978a1fb866c888
Sushant.sinha_94 <sushant1058jan@gmail.com>
2022-11-16 00:33:26 +0530

    dsa

--- DSA/Stack&Queue.py
+++ DSA/Stack&Queue.py
@@ -307,7 +307,82 @@
 #Queue using Linked Lists
 
 
-
-
+class Node:
+    def __init__(self,value=None):
+        self.value=value
+        self.next = None
     
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None    
         
+    def __iter__(self):
+        currentNode=self.head
+        while currentNode:
+            yield currentNode
+            currentNode=currentNode.next
+            
+            
+class Queue:
+    def __init__(self):
+        self.LinkedList=LinkedList()
+        
+    def __str__(self):
+        values=[str(x) for x in self.LinkedList]
+        return ' '.join(values)
+        
+    def enqueue(self,value):
+        newNode=Node(value)
+        if self.LinkedList.head == None:
+            self.LinkedList.head=newNode
+            self.LinkedList.tail=newNode
+        else:
+            self.LinkedList.tail.next=newNode
+            self.LinkedList.tail=newNode
+            
+    def isEmpty(self):
+        if self.LinkedList.head==None:
+            return True
+        else:
+            return False
+        
+            
+    def dequeue(self):
+        if self.isEmpty():
+            return "Queue is Empty!"
+        elif (self.LinkedList.head==self.LinkedList.tail):  #checking if there is only one node
+            firstElement=self.LinkedList.head
+            self.LinkedList.head==None
+            self.LinkedList.tail==None
+        else:
+            firstElement=self.LinkedList.head
+            self.LinkedList.head=self.LinkedList.head.next
+        return firstElement
+    
+    
+    def peek(self):
+        if self.isEmpty():
+            return "Queue is empty!"
+        else:
+            return self.LinkedList.head
+        
+    def delete(self):
+        self.LinkedList.head==None
+        self.LinkedList.tail==None
+        return "Queue deleted!"
+        
+        
+        
+    
+customQueue=Queue()
+customQueue.enqueue(4)
+customQueue.enqueue(45)
+customQueue.enqueue(40)
+print(customQueue)
+print(customQueue.dequeue())
+print(customQueue)       
